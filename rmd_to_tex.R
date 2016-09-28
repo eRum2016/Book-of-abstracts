21388391104e23e80ee7e445ba734f560f519006
Nowosad <tupiszakaczki@gmail.com>
2016-09-28 14:08:41 -0400

    first full version vol08

--- rmd_to_tex.R
+++ rmd_to_tex.R
@@ -5,5 +5,5 @@ library('tidyverse')
 fl <- dir('abstracts_rmd/', full.names = TRUE)
 
 safe_render <- safely(render)
-fl %>% map(~safe_render(input=., output_dir='abstracts'))
+fl %>% map(~safe_render(input=., output_dir='abstracts2'))
 system('cd abstracts; find . -name "*.pdf" -type f -delete')

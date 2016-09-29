5b4db5cf4e0c42c6b9a366f0d8a424fc854913a8
Nowosad <tupiszakaczki@gmail.com>
2016-09-29 11:58:05 -0400

    cleaned old files

--- rmd_to_tex.R
+++
@@ -1,9 +0,0 @@
-# devtools::install_github('eRum2016/erum')
-library('rmarkdown')
-library('tidyverse')
-
-fl <- dir('abstracts_rmd/', full.names = TRUE)
-
-safe_render <- safely(render)
-fl %>% map(~safe_render(input=., output_dir='abstracts2'))
-system('cd abstracts; find . -name "*.pdf" -type f -delete')

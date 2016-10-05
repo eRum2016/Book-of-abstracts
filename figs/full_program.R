67b96767a29cf7124a85fa5eb5cd7bd394fccee6
Nowosad <tupiszakaczki@gmail.com>
2016-10-05 18:42:57 -0400

    maciejs ideas

--- figs/full_program.R
+++ figs/full_program.R
@@ -2,9 +2,9 @@ library('readxl')
 library('purrr')
 library('xtable')
 
-program <- read_excel('figs/Preliminary_program.xlsx', sheet=2)[c(1:84), c(1:6)]
-names(program) <- c("Session", "Type", "Name", "Surname", "Title", "Chairman")
+program <- read_excel('figs/Preliminary_program.xlsx', sheet=2)[c(1:84), c(1, 3:6)]
+names(program) <- c("Session", "Name", "Surname", "Title", "Chairman")
 x <- xtable(program, digits=0)
-align(x) <- "p{0.1cm}|p{2.6cm}|p{1.8cm}|p{1.9cm}|p{2.2cm}|p{9cm}|p{2.5cm}|"
+align(x) <- "p{0.01cm}|p{2.6cm}|p{1.7cm}|p{2.0cm}|p{6.5cm}|p{1.5cm}|"
 print(x, type="latex", file="figs/full_program.tex", include.rownames=FALSE, 
       tabular.environment='longtable', floating=FALSE, size="\\fontsize{9pt}{10pt}\\selectfont")
--- figs/workshops_location.R
+++ figs/workshops_location.R
@@ -4,6 +4,6 @@ library('xtable')
 
 work <- read_excel('figs/workshops_location.xlsx')
 x <- xtable(work, digits=0)
-align(x) <- "p{0.1cm}|p{12cm}|p{3cm}|"
+align(x) <- "p{0.1cm}|p{11.1cm}|p{4.6cm}|"
 print(x, type="latex", file="figs/workshops_location.tex", include.rownames=FALSE, 
       tabular.environment='longtable', floating=FALSE, size="\\fontsize{9pt}{10pt}\\selectfont")

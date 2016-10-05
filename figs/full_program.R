9c547da0688a31cd7c7424fa14530f54388a8c5b
Nowosad <tupiszakaczki@gmail.com>
2016-10-05 13:00:03 -0400

    full program updated

--- figs/full_program.R
+++ figs/full_program.R
@@ -2,7 +2,7 @@ library('readxl')
 library('purrr')
 library('xtable')
 
-program <- read_excel('figs/Preliminary_program.xlsx', sheet=2)[c(1:73), c(1:6)]
+program <- read_excel('figs/Preliminary_program.xlsx', sheet=2)[c(1:84), c(1:6)]
 names(program) <- c("Session", "Type", "Name", "Surname", "Title", "Chairman")
 x <- xtable(program, digits=0)
 align(x) <- "p{0.1cm}|p{2.6cm}|p{1.8cm}|p{1.9cm}|p{2.2cm}|p{9cm}|p{2.5cm}|"

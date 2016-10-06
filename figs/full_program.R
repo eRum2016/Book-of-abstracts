8eceaa24343979fce097db73a390ba7f92f17eca
Nowosad <tupiszakaczki@gmail.com>
2016-10-06 10:53:21 -0400

    b6paper workshops and venues added

--- figs/full_program.R
+++ figs/full_program.R
@@ -5,6 +5,6 @@ library('xtable')
 program <- read_excel('figs/Preliminary_program.xlsx', sheet=2)[c(1:84), c(1, 3:6)]
 names(program) <- c("Session", "Name", "Surname", "Title", "Chairman")
 x <- xtable(program, digits=0)
-align(x) <- "p{0.01cm}|p{2.6cm}|p{1.7cm}|p{2.0cm}|p{6.5cm}|p{1.5cm}|"
+align(x) <- "p{0.01cm}|p{2.2cm}|p{1.5cm}|p{1.7cm}|p{4cm}|p{1.5cm}|"
 print(x, type="latex", file="figs/full_program.tex", include.rownames=FALSE, 
       tabular.environment='longtable', floating=FALSE, size="\\fontsize{9pt}{10pt}\\selectfont")
--- figs/workshops_location.R
+++ figs/workshops_location.R
@@ -3,7 +3,19 @@ library('purrr')
 library('xtable')
 
 work <- read_excel('figs/workshops_location.xlsx')
-x <- xtable(work, digits=0)
-align(x) <- "p{0.1cm}|p{11.1cm}|p{4.6cm}|"
-print(x, type="latex", file="figs/workshops_location.tex", include.rownames=FALSE, 
+
+m_work <- work[c(1:5), ]
+a_work <- work[c(6:10), ]
+
+names(m_work) <- c("Morning workshops (9:00 - 12:30)", "Location")
+names(a_work) <- c("Afternoon workshops (9:00 - 12:30)", "Location")
+
+x1 <- xtable(m_work, digits=0)
+align(x1) <- "p{0.1cm}|p{8.2cm}|p{4.6cm}|"
+print(x1, type="latex", file="figs/m_workshops_location.tex", include.rownames=FALSE, 
+      tabular.environment='longtable', floating=FALSE, size="\\fontsize{9pt}{10pt}\\selectfont")
+
+x2 <- xtable(a_work, digits=0)
+align(x2) <- "p{0.1cm}|p{8.2cm}|p{4.6cm}|"
+print(x2, type="latex", file="figs/a_workshops_location.tex", include.rownames=FALSE, 
       tabular.environment='longtable', floating=FALSE, size="\\fontsize{9pt}{10pt}\\selectfont")
